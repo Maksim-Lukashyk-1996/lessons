cc3b910e3493b9a538bb7e190aefa537bc3bc49d
Maksim <Maksim>
2023-11-22 15:03:42 +0300

    If

--- src/If.java
+++ src/If.java
@@ -1,9 +1,12 @@
 public class If {
     public static void main(String[] args) {
-        if (4 == 5) {
+        int myInt = 15;
+        if (myInt < 10) {
             System.out.println("Да, верно.");
-        } else{
+        } else if(myInt > 20){
             System.out.println("Нет, не верно.");
+        } else{
+            System.out.println("Ни один из предыдущих случаев.");
         }
     }
 }
