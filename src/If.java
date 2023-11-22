79e3c5de4a2bbac32ef2ab683bf40cde0644501d
Maksim <Maksim>
2023-11-22 15:05:32 +0300

    If changed

--- src/If.java
+++ src/If.java
@@ -1,9 +1,9 @@
 public class If {
     public static void main(String[] args) {
-        int myInt = 15;
+        int myInt = 5;
         if (myInt < 10) {
             System.out.println("Да, верно.");
-        } else if(myInt > 20){
+        } else if(myInt < 20){
             System.out.println("Нет, не верно.");
         } else{
             System.out.println("Ни один из предыдущих случаев.");
