93ef04e93171c25d3418fc8f83989f49e17f4d06
Maksim <Maksim>
2023-11-23 14:07:47 +0300

    Cycle DoWhile

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 public class DoWhile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите 5: ");
-        int value = scanner.nextInt();
-        while(value != 5){
-            System.out.println("ВВЕДИТЕ 5! : ");
+        int value;
+
+        do{
+            System.out.println("Введите пароль: ");
             value = scanner.nextInt();
-        }
-        System.out.println("ОК");
+
+            if (value == 993513){
+                System.out.println("ОК");
+            } else{
+                System.out.println("Неверный пароль.");
+            }
+
+        } while(value != 993513); // Отличие цикла: while, от do while. While-сначала выполняет условие, а затем проверяет инструкции.
+        // do while-сначала выполняет инструкции, а потом проверяет условие.
     }
 }
